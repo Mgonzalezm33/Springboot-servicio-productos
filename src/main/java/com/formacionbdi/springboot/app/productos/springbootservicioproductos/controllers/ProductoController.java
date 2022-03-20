634e3f3e40a1f2b57e8f0c9d69be077fc5f2d5bf
mgonzalezm33 <moigonzales1061@gmail.com>
2022-03-20 00:05:27 -0600

    servicio productos

---
+++ src/main/java/com/formacionbdi/springboot/app/productos/springbootservicioproductos/controllers/ProductoController.java
@@ -0,0 +1,29 @@
+package com.formacionbdi.springboot.app.productos.springbootservicioproductos.controllers;
+
+import java.util.List;
+
+import com.formacionbdi.springboot.app.productos.springbootservicioproductos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.springbootservicioproductos.models.service.IProductoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductoController {
+    
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/listar")
+    public List<Producto> listar(){ 
+        return productoService.findAll();
+    }
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id){
+        return productoService.findById(id);
+    }
+
+}
